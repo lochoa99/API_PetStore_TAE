b7258a1eece2f12cdedf7d4a952afc81fd309770
l.ochoa <l.ochoa@globant.com>
2026-03-18 16:42:31 -0500

    Add User and Order POJO payload classes

--- src/test/java/api/payloads/User.java
+++ src/test/java/api/payloads/User.java
@@ -1,4 +1,83 @@
 package api.payloads;
 
+/**
+ * Clase POJO (Plain Old Java Object) para el modelo de datos de un Usuario.
+ * Contiene los atributos, getters y setters necesarios para armar el JSON.
+ */
 public class User {
+
+    private int id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private int userStatus;
+
+    // --- Getters y Setters ---
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(int userStatus) {
+        this.userStatus = userStatus;
+    }
 }
